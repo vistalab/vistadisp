e01ed04d5b6f39e202a76c93fcafab2c7c36e822
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-02-07 09:53:08 -0500

    created runme directory to store scripts for running experiments

---
+++ runme/runme_MEG.m
@@ -0,0 +1,72 @@
+%RUNME MEG 
+%
+% MEG Full-field flicker experiment (steady state)
+% ------
+%   Run time per experiment = XX s
+%   XX s (XX-s countdown + XX s)   
+%   XX cycles at XX s each
+
+%% Clear
+% clear all; close all;
+
+addpath(genpath('~/matlab/MEG/PTBWrapper/'))
+
+% initialize stim tracker for MEG
+try PTBInitStimTracker; end
+global PTBTriggerLength 
+PTBTriggerLength = 0.001;
+% debug mode
+PsychDebugWindowConfiguration
+
+
+%% Calibration
+% cal = 'CBI_NYU_projector';
+cal = 'meg_lcd';
+d   = loadDisplayParams(cal);
+hz  = FrameRate(d.screenNumber);
+tr  = 1/hz*60;
+
+% Default parameters
+params = retCreateDefaultGUIParams;
+
+
+%% Full field ONOFF
+params.modality         = 'MEG'; 
+params.prescanDuration  = 0;
+params.interleaves      = NaN;
+params.tr               = 1/hz*60;
+params.calibration      = cal;
+params.framePeriod      = tr;
+params.startScan        = 0;
+params.motionSteps      = 2;
+params.tempFreq         = 6/tr;
+params.repetitions      = 1;
+params.experiment       = 'full-field, on-off';
+params.period           = 12*params.tr;
+params.numCycles        = 6;
+
+
+
+%% ********************
+%  ***** GO ***********
+%  *********************
+
+P = ret(params);
+
+%% Check timing results
+f = dir('~/Desktop/2014*.mat');
+load(fullfile('~', 'Desktop', f(end).name));
+figure(101); clf
+
+% desired inter-stimulus duration
+plot(diff(stimulus.seqtiming));
+
+% measured inter-stimulus duration
+hold on; plot(diff(response.flip), 'r-'); 
+
+ylim(median(diff(response.flip)) + [-.02 .02])
+% frames between stimuli
+frames = round(diff(response.flip) / (1/60)); 
+
+% how many interstimulus frames differed from the median?
+disp(sum(frames ~= median(frames)))
