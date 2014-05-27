5df7966cc733d6bf844f4474bcda500ebc2b1d1d
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-05-27 11:33:32 -0400

    runme function

---
+++ runme/runme_MEG_OnOffLeftRight.m
@@ -0,0 +1,77 @@
+function runme_MEG_OnOffLeftRight(n)
+%RUNME MEG2_OnOffLeftRight
+%
+% MEG Full-field on-off, left/right flicker experiment (steady state)
+% ------
+%   Run time per experiment = 72 seconds
+%   6 cycles at 12 s each
+%   6 cycles are randomly orderd full-full-left-left-right-right, with
+%       blanks between each
+
+%% go
+% cd /Users/megadmin/Desktop/Experiments/Winawer/
+% addpath(genpath('vistadisp'))
+
+%% 
+% initialize stim tracker for MEG
+PTBInitStimTracker;
+global PTBTriggerLength 
+PTBTriggerLength = 0.001;
+
+% debug mode?
+%  PsychDebugWindowConfiguration
+
+
+%% Calibration
+cal = 'meg_lcd';
+d   = loadDisplayParams(cal);
+hz  = FrameRate(d.screenNumber);
+tr  = 1/hz*60;
+
+
+
+%% Default parameters
+params = retCreateDefaultGUIParams;
+
+
+%% Hemifield and ONOFF mixture
+params.modality         = 'MEG'; 
+params.prescanDuration  = 0;
+params.interleaves      = NaN;
+params.tr               = 1/hz*60;
+params.calibration      = cal;
+params.framePeriod      = tr;
+params.startScan        = 0;
+params.motionSteps      = 2;
+params.tempFreq         = 6/tr;
+params.repetitions      = 1;
+params.experiment       = 'Experiment From File';
+params.period           = 12*params.tr;
+params.numCycles        = 6;
+
+
+
+
+%% ********************
+%  ***** GO ***********
+%  *********************
+params.loadMatrix = sprintf('attention_onOffLeftRight_params%d.mat', n);
+ret(params);
+
+%% Check timing results
+f = dir('~/Desktop/2014*.mat');
+load(fullfile('~', 'Desktop', f(end).name));
+figure(101); clf
+
+% desired inter-stimulus duration
+plot(diff(stimulus.seqtiming));
+
+% measured inter-stimulus duration
+hold on; plot(diff(response.flip), 'r-'); 
+
+ylim(median(diff(response.flip)) + [-.001 .001])
+% frames between stimuli
+frames = round(diff(response.flip) / (1/60)); 
+
+% how many interstimulus frames differed from the median?
+disp(sum(frames ~= median(frames)))
