ce466128470b1d915f2cbe81dc7f254d1bee8e74
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-05-27 10:50:03 -0400

    updates to vTPJ experiment

---
+++ Applications2/Retinotopy/standard/runme_vTPJ_attention_2014_05_26_wl_002.m
@@ -0,0 +1,235 @@
+% runme_vTPJ_attention_2014_05_26_wl_002
+
+% Script to make structure for vTPJ detection experiment
+
+% Create a structure for 'Experiment from file'. Populating this structure
+%   is the main purpose of the script
+stimulus = struct('images', [], 'cmap', [], 'seq', [], ...
+    'seqtiming', [], 'fixSeq', [], 'scrRect', [], 'destRect', []);
+
+% *******************************
+% **** Settable parameters ******
+% *******************************
+
+% screen diameter (pixels)
+sz = 768;
+
+% refresh rate (seconds)
+dt = 1/60 * 6; 
+
+% scanner tr, also duration of trial
+tr = 2;
+
+% isi (fixed or random?)
+ISI = [2 3 4];
+
+% experiment duration (seconds)
+num_trs = 135;
+
+% noise level
+noise_amp = 0.2; % [0 1]
+
+% Gabor parameters
+gabor.amplitudes = [0 .04 .08 .12 .2 .4]; % contrast (max is 1)
+gabor.cpfov      = 10;    % cycles per field of view
+gabor.bandwidth  = 2;     % number of cycles per 4 std dev of the Gaussian envelope
+gabor.ecc        = 0.8;   % eccentricity of g center relative to screen radius
+num_positions    = 2;
+gabor.angles     = (0:num_positions-1)/num_positions * 2*pi - pi/2;
+
+% *******************************
+% **** Derived parameters *******
+% *******************************
+T                = num_trs * tr; % experiment length (seconds)
+num_images       = T / dt;      % for whole experiment
+images_per_trial = tr / dt;      % for one trial
+
+%   grid for Gabors
+[xx, yy]    = meshgrid((-sz/2+.5:sz/2-.5)/(sz/2), (-sz/2+.5:sz/2-.5)/(sz/2));  % x,y grid
+
+% Gabor row and column centers
+[r, c] = pol2cart(gabor.angles, gabor.ecc * sz);
+gabor.rows = r + sz/2;
+gabor.cols = c + sz/2;
+% figure; plot(gabor.cols([1:end 1]),gabor.rows([1:end 1]), 'o-', [sz/2 sz/2], [0 sz], 'k-', [0 sz],[sz/2 sz/2],  'k-'); axis([0 sz 0 sz])
+
+% *******************************
+% **** Make images *******
+% *******************************
+
+% to conserve space, limit the number of noise images. 
+num_noise_images = 100;
+
+% Background images 
+noise_images = uint8(randn(sz, sz, num_noise_images)*128*noise_amp+128);
+
+% For bookkeeping, make iniital stimulus sequence a matrix of images x
+% trials
+seq = zeros(images_per_trial, num_trs);
+
+% Random noise patterns for each image. Afterward we will replace trial
+% images with combination of noise and target
+for ii = 1:num_trs, seq(:,ii) = randperm(num_noise_images, images_per_trial);  end
+
+% designate time points for trials, leaving 12 s of blank at start and end
+this_tr = ceil(13/tr); ii = 1;
+ISIs = ISI(randintrange(1,length(ISI),[1 1000]));
+trial = [];
+while this_tr < num_trs - ceil(12/tr)
+    trial.tr(ii) = this_tr;
+    ii = ii+1;
+    this_tr = this_tr + ISIs(ii);
+end
+num_trials = ii-1;
+
+% for each trial, assign parameters
+rand_assign = @(x, n) x(randintrange(1,length(x), [1 n]));
+
+%   rows and cols of gabor center
+position_ind = rand_assign(1:length(gabor.rows), num_trials);
+trial.row = gabor.rows(position_ind);
+trial.col = gabor.cols(position_ind);
+
+%   phase and angle of gabor sinusoid
+trial.ang = rand(1, num_trials) * 2 * pi;
+trial.ph  = rand(1, num_trials) * 2 * pi;
+trial.amplitudes = rand_assign(gabor.amplitudes, num_trials);
+
+
+% Make the images for each trial
+%   temporal envelope - this is the same for every trial
+%   envelope = hann(images_per_trial);
+envelope = hann(round(images_per_trial/2));
+envelope = padarray(envelope, round(images_per_trial/4), 'pre');
+envelope = padarray(envelope, images_per_trial-length(envelope), 'post');
+envelope = hann(round(images_per_trial));
+envelope_onset = find(envelope,1);
+images = noise_images;
+for ii = 1:num_trials
+    % make the gabor for this trial
+    G = makegabor2d(sz,trial.row(ii),trial.col(ii),gabor.cpfov,trial.ang(ii),trial.ph(ii),gabor.bandwidth,xx,yy) ;
+    
+    % precompute images
+    these_images = double(noise_images(:,:, randperm(num_noise_images, images_per_trial)));
+    for jj = 1:images_per_trial
+        
+        these_images(:,:,jj) = these_images(:,:,jj) + envelope(jj) * G * trial.amplitudes(ii) * 128;
+        
+    end
+    
+    these_images = uint8(these_images);
+    seq(:,trial.tr(ii)) = (1:images_per_trial) + size(images,3);
+    images = cat(3, images, these_images);
+        
+%     figure(1); for jj = 1:images_per_trial; imshow(images(:,:,jj)); title(sprintf('Trial %d\tImage %d', ii, jj)); pause(.01); end
+%     waitforbuttonpress
+end
+
+
+
+%% Store variables in struct and save
+stimulus.images     = images;
+stimulus.seq        = seq(:);
+stimulus.seqtiming  = (0:length(stimulus.seq)-1)' * dt;
+stimulus.cmap       = (0:255)'*[1 1 1];
+stimulus.fixSeq     = randi(68,size(stimulus.seq));
+stimulus.srcRect    = [0 0 sz sz];
+stimulus.destRect   = [0 0 sz sz];
+stimulus.trial_info = trial;
+
+% change fixation sequence every n images
+fix_dur = 2;
+for ii = 2:fix_dur
+    stimulus.fixSeq(ii:fix_dur:end) = stimulus.fixSeq(1:fix_dur:end);
+end
+% save
+pth = '~/matlab/git/vistadisp/Applications2/Retinotopy/standard/storedImagesMatrices/';
+fname_stim = 'tpj_attention_params10.mat';
+
+save(fullfile(pth, fname_stim), 'stimulus');
+
+
+%%
+
+
+params = retCreateDefaultGUIParams;
+params.experiment      = 'experiment from file';
+params.calibration     = 'CBI_NYU_projector';
+params.loadMatrix      = fname_stim;
+params.tr              = tr;
+params.period          = T;
+params.triggerKey      = '`';
+params.prescanDuration = 0;
+params.fixation        = 'rsvp letters';
+
+%PsychDebugWindowConfiguration
+p = ret(params);
+
+%% check accuracy
+num_runs = 10;
+accuracy = NaN(length(contrasts), 100, num_runs);
+
+for this_run = 1:num_runs
+    
+    d = dir('~/Desktop/*.mat');
+    S = [d(:).datenum];
+    [~,S] = sort(S);
+    results = load(fullfile('~/Desktop', d(S(end-num_runs+this_run)).name));
+    tmp = load(fullfile(pth, sprintf('tpj_attention_params%02d.mat',this_run)));
+    trial_data = tmp.stimulus.trial_info; clear tmp;
+    % get the unique response keys
+    inds     = find(results.response.keyCode);
+    keycodes = unique(results.response.keyCode(inds));
+    chars    = KbName(keycodes);
+    numeric  = cellfun(@(x) sscanf(x, '%d'), chars, 'UniformOutput', false);
+    resp(1)  = keycodes(cellfind(numeric, 4));
+    resp(2)  = keycodes(cellfind(numeric, 3));
+    
+    
+    
+    responses(1,:) = results.response.keyCode == resp(1);
+    responses(2,:) = results.response.keyCode == resp(2);
+    
+    %
+    figure(100+this_run); clf
+    set(gca, 'Color', 'k'); hold on
+    %plot(responses(1,:), 'r-'); hold on
+    
+    
+    gabor_onsets = (trial_data.tr-1) * images_per_trial+1;
+    set(gca, 'XTick', gabor_onsets, 'XGrid', 'on')
+    
+    contrasts = unique(trial_data.amplitudes);
+    colors = jet(length(contrasts));
+    
+    gabor_detections = find(responses(2,:));
+    
+    for ii = 1:length(contrasts)
+        indices = find(trial_data.amplitudes == contrasts(ii));
+        for jj = 1:length(indices)
+            x = (trial_data.tr(indices(jj))-1) * images_per_trial+envelope_onset;
+            plot([x x], [0 1], '-', 'Color', colors(ii,:), 'LineWidth', ii);
+            
+            accuracy(ii,jj, this_run) = any(gabor_detections - x > 0 & gabor_detections - x < 1.5/dt);
+        end
+        
+    end
+    
+    
+    
+    plot(responses(2,:), 'g--');
+    
+    
+end
+accuracy = reshape(accuracy, length(contrasts), []);
+psychometric_function = nanmean(accuracy,2);
+psychometric_sem      = sqrt(psychometric_function .* (1-psychometric_function) ./ sum(isfinite(accuracy),2));
+
+%%
+
+figure(2); clf; set(gcf, 'Color', 'w')
+set(gca, 'FontSize', 20)
+errorbar(contrasts, psychometric_function, psychometric_sem, 'r-o', 'LineWidth', 2, 'MarkerSize', 12)
+axis tight
+xlabel('Contrast')
+ylabel('Accuracy')
