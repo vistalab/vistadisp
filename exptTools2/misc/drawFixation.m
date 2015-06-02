10d3f82c904dc80ab496a9c90c35c41910aca86e
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-06-02 16:54:23 -0400

    exptTools2/experimentControl/showScanStimulus.m

--- exptTools2/misc/drawFixation.m
+++ exptTools2/misc/drawFixation.m
@@ -45,7 +45,7 @@ switch(lower(d.fixType))
         
     case{'digits'}
         % when digits are ranging from 0 to 9
-        display_digit = mod(colIndex, 10)
+        display_digit = mod(colIndex, 10);
         % colIndex ranging from 0 to 19 for digits 0-9 in black and white, blank when colIndex = 20
         if colIndex < 20
             %Screen('DrawText', d.windowPtr, num2str(display_digit), d.fixX, d.fixY,  d.fixColorRgb(colIndex+1,:));
